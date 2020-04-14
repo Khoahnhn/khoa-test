7426bc476e17c31dc92b2403b190cd5405283ec7
KhoaLt <khoalt.pht@gmail.com>
2020-04-15 02:01:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route');
+
+var port = 3000;
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'))
+
+app.get('/', function(req,res) {
+	res.render('index',{
+		name: 'AAA'
+	});
+});
+
+app.use('/users',userRoute);
+
+app.listen(port,function() {
+	console.log('Server listening on port ' + port)
+});
